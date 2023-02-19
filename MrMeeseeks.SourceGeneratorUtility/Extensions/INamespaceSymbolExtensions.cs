c2b6070b5b8f27ff5a4eda6f640dd9d94955bb28
Dima Enns <dieter.enns@gmail.com>
2023-02-19 12:27:48 +0100

    Some functions for ITypeSymbol and INamespaceSymbol

---
+++ MrMeeseeks.SourceGeneratorUtility/Extensions/INamespaceSymbolExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MrMeeseeks.SourceGeneratorUtility.Extensions;
+
+// ReSharper disable once InconsistentNaming
+public static class INamespaceSymbolExtensions
+{
+    // Picked from https://github.com/YairHalberstadt/stronginject Thank you!
+    internal static string FullName(this INamespaceSymbol @namespace) =>
+        @namespace.ToDisplayString(new SymbolDisplayFormat(typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces));
+}
