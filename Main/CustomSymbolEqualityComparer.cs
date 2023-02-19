9036cdd58bbd587fa6fb6a5bbc63c9092c09bec1
Dima Enns <dieter.enns@gmail.com>
2023-02-19 15:32:41 +0100

    Implement a custom SymbolEqualityComparer which compares implementations of INamedTypeSymbols by relevant values

---
+++ Main/CustomSymbolEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MrMeeseeks.SourceGeneratorUtility;
+
+public sealed class CustomSymbolEqualityComparer : IEqualityComparer<ISymbol?>
+{
+    private readonly SymbolEqualityComparer _originalSymbolEqualityComparer;
+    
+    public static readonly CustomSymbolEqualityComparer Default = new (false);
+    public static readonly CustomSymbolEqualityComparer IncludeNullability = new (true);
+
+    internal CustomSymbolEqualityComparer(bool considerNullability) =>
+        _originalSymbolEqualityComparer = considerNullability
+            ? SymbolEqualityComparer.IncludeNullability
+            : SymbolEqualityComparer.Default;
+
+    public bool Equals(ISymbol? x, ISymbol? y)
+    {
+        if (x is not INamedTypeSymbol xNamed || y is not INamedTypeSymbol yNamed
+            || x is IErrorTypeSymbol || y is IErrorTypeSymbol) 
+            return _originalSymbolEqualityComparer.Equals(x, y);
+
+        return _originalSymbolEqualityComparer.Equals(xNamed.ContainingNamespace, yNamed.ContainingNamespace) 
+               && xNamed.Name == yNamed.Name 
+               && xNamed.TypeArguments.Length == yNamed.TypeArguments.Length 
+               && xNamed.TypeArguments.Zip(yNamed.TypeArguments, Equals).All(b => b);
+    }
+
+    public int GetHashCode(ISymbol? obj) => _originalSymbolEqualityComparer.GetHashCode(obj);
+}
