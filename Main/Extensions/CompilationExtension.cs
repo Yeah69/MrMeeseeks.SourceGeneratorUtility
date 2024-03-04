b983647435358cd42c558c99a86343248e572c23
Dima Enns <dieter.enns@gmail.com>
2024-03-04 12:43:23 +0100

    Improve exception message in case metadata name wasn't found

--- Main/Extensions/CompilationExtension.cs
+++ Main/Extensions/CompilationExtension.cs
@@ -5,6 +5,6 @@ namespace MrMeeseeks.SourceGeneratorUtility.Extensions;
 public static class CompilationExtension
 {
     public static INamedTypeSymbol GetTypeByMetadataNameOrThrow(this Compilation compilation, string metadataName) =>
-        compilation.GetTypeByMetadataName(metadataName) 
-        ?? throw new ArgumentException("Type not found by metadata name.", nameof(metadataName));
+        compilation.GetTypeByMetadataName(metadataName)
+        ?? throw new ArgumentException($"Type not found by metadata name \"{metadataName}\".", nameof(metadataName));
 }
