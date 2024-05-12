ceb5e67cc10a59d6e9e8a740ca6f3fae30df6d8e
Dima Enns <dieter.enns@gmail.com>
2024-05-12 13:05:03 +0200

    Extended CustomSymbolEqualityComparer by more sophisticated type parameter comparison
    It now only checks the equality of the name and considers equality of  the declaring methods and declaring types

--- Main/CustomSymbolEqualityComparer.cs
+++ Main/CustomSymbolEqualityComparer.cs
@@ -10,7 +10,7 @@ public sealed class CustomSymbolEqualityComparer : IEqualityComparer<ISymbol?>
     public static readonly CustomSymbolEqualityComparer Default = new (false);
     public static readonly CustomSymbolEqualityComparer IncludeNullability = new (true);
 
-    internal CustomSymbolEqualityComparer(bool considerNullability)
+    private CustomSymbolEqualityComparer(bool considerNullability)
     {
         _considerNullability = considerNullability;
         _originalSymbolEqualityComparer = considerNullability
@@ -20,19 +20,39 @@ public sealed class CustomSymbolEqualityComparer : IEqualityComparer<ISymbol?>
 
     public bool Equals(ISymbol? x, ISymbol? y)
     {
-        if (x is not INamedTypeSymbol xNamed || y is not INamedTypeSymbol yNamed
-            || x is IErrorTypeSymbol || y is IErrorTypeSymbol) 
-            return _originalSymbolEqualityComparer.Equals(x, y);
+        return (x, y) switch
+        {
+            (INamedTypeSymbol xN, INamedTypeSymbol yN) => InnerEqualsNamed(xN, yN),
+            (ITypeParameterSymbol xT, ITypeParameterSymbol yT) => InnerEqualsTypeParameter(xT, yT),
+            _ => _originalSymbolEqualityComparer.Equals(x, y)
+        };
 
-        return _originalSymbolEqualityComparer.Equals(xNamed.ContainingNamespace, yNamed.ContainingNamespace) 
-               && xNamed.Name == yNamed.Name 
-               && xNamed.TypeArguments.Length == yNamed.TypeArguments.Length 
-               && xNamed.TypeArguments.Zip(yNamed.TypeArguments, Equals).All(b => b)
-               && (!_considerNullability 
-                   // either both annotated
-                   || xNamed.NullableAnnotation == NullableAnnotation.Annotated && yNamed.NullableAnnotation == NullableAnnotation.Annotated
-                   // or both not annotated
-                   || xNamed.NullableAnnotation != NullableAnnotation.Annotated && yNamed.NullableAnnotation != NullableAnnotation.Annotated);
+        bool InnerEqualsNamed(INamedTypeSymbol xNamed, INamedTypeSymbol yNamed)
+        {
+            if (xNamed is IErrorTypeSymbol || yNamed is IErrorTypeSymbol) 
+                return _originalSymbolEqualityComparer.Equals(x, y);
+            
+            return _originalSymbolEqualityComparer.Equals(xNamed.ContainingNamespace, yNamed.ContainingNamespace) 
+                   && xNamed.Name == yNamed.Name 
+                   && xNamed.TypeArguments.Length == yNamed.TypeArguments.Length 
+                   && xNamed.TypeArguments.Zip(yNamed.TypeArguments, Equals).All(b => b)
+                   && (!_considerNullability 
+                       // either both annotated
+                       || xNamed.NullableAnnotation == NullableAnnotation.Annotated && yNamed.NullableAnnotation == NullableAnnotation.Annotated
+                       // or both not annotated
+                       || xNamed.NullableAnnotation != NullableAnnotation.Annotated && yNamed.NullableAnnotation != NullableAnnotation.Annotated);
+        }
+
+        bool InnerEqualsTypeParameter(ITypeParameterSymbol xTypeParameter, ITypeParameterSymbol yTypeParameter) =>
+            xTypeParameter.Name == yTypeParameter.Name
+            && (!_considerNullability 
+                // either both annotated
+                || xTypeParameter.NullableAnnotation == NullableAnnotation.Annotated && yTypeParameter.NullableAnnotation == NullableAnnotation.Annotated
+                // or both not annotated
+                || xTypeParameter.NullableAnnotation != NullableAnnotation.Annotated && yTypeParameter.NullableAnnotation != NullableAnnotation.Annotated)
+            && (xTypeParameter.DeclaringMethod is {} xDeclaringMethod && yTypeParameter.DeclaringMethod is {} yDeclaringMethod && Equals(xDeclaringMethod, yDeclaringMethod) 
+                || xTypeParameter.DeclaringType is {} xDeclaringType && yTypeParameter.DeclaringType is {} yDeclaringType && Equals(xDeclaringType, yDeclaringType) 
+                || xTypeParameter is { DeclaringType: null, DeclaringMethod: null } && yTypeParameter is { DeclaringType: null, DeclaringMethod: null });
     }
 
     public int GetHashCode(ISymbol? obj) => _originalSymbolEqualityComparer.GetHashCode(obj);
